89c68eccec8ea45d1ceb5388f9dbaab315bb6d96
soywiz <soywiz@gmail.com>
2024-03-15 20:50:20 +0100

    Try to skip problematic I/O wasm tests

--- korlibs-audio/test/korlibs/SkipIOTest.kt
+++ korlibs-audio/test/korlibs/SkipIOTest.kt
@@ -1,5 +1,6 @@
 import korlibs.platform.*
 
-val skipIOTest: Boolean get() = Platform.isJs || Platform.isAndroid
+val skipIOTest: Boolean get() = Platform.isJs || Platform.isWasm || Platform.isAndroid
+//val skipIOTest: Boolean get() = Platform.isAndroid || Platform.isWasm
 //val skipIOTest: Boolean get() = OS.isAndroid
 val doIOTest: Boolean get() = !skipIOTest
