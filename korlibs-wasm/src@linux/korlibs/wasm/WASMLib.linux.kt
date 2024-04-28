ff070ba634e404c239a7c787411029e85776c299
soywiz <soywiz@gmail.com>
2024-04-28 20:47:45 +0200

    Some fixes

--- korlibs-wasm/src@linux/korlibs/wasm/WASMLib.linux.kt
+++ korlibs-wasm/src@linux/korlibs/wasm/WASMLib.linux.kt
@@ -1,5 +1,3 @@
 package korlibs.wasm
 
 actual open class WASMLib actual constructor(content: ByteArray) : IWASMLib by DefaultNativeWASMLib(content)
-
-open class DefaultNativeWASMLib(content: ByteArray) : IWASMLib, BaseWASMLib(content)
