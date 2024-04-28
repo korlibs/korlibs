4c3cfb6224a0c59c77a593b92f8645aea8721267
soywiz <soywiz@gmail.com>
2024-04-28 21:11:03 +0200

    Some fixes

--- korlibs-image/test/korlibs/image/format/WEBPTest.kt
+++ korlibs-image/test/korlibs/image/format/WEBPTest.kt
@@ -8,12 +8,12 @@ import kotlin.test.*
 class WEBPTest {
     @Test
     fun test1() = suspendTest {
+        if (!WEBP.isAvailable) return@suspendTest
         if (Platform.isWasm) { println("Skipping WEBPTest WASM for now as it hangs: https://github.com/korlibs/korge/pull/2057#issuecomment-1837089337"); return@suspendTest }
         if (Platform.isIos || Platform.isTvos) {
             println("Skipping WEBPTest for now on iOS")
             return@suspendTest
         }
-        WEBP()
         val bmp = resourcesVfs["Exif5-2x.webp"].readBitmap(ImageDecodingProps(format = WEBP(), preferKotlinDecoder = true))
         assertEquals("256x256", "${bmp.size}")
     }
