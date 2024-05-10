3795974f648118271cf9d6e2ee1a70e55ccc0d08
soywiz <soywiz@gmail.com>
2024-05-10 11:41:42 +0200

    Some adjustments

--- korlibs-io/src@jvm/korlibs/io/stream/AsyncStreamExt.jvm.kt
+++ korlibs-io/src@jvm/korlibs/io/stream/AsyncStreamExt.jvm.kt
@@ -1,26 +1,26 @@
 package korlibs.io.stream
 
+import korlibs.io.async.*
 import korlibs.io.lang.*
+import kotlinx.coroutines.*
 import java.io.*
+import kotlin.coroutines.*
 
-fun InputStream.toAsync(length: Long? = null): AsyncInputStream {
+fun InputStream.toAsync(context: CoroutineContext? = Dispatchers.IO, length: Long? = null): AsyncInputStream {
 	val syncIS = this
-	if (length != null) {
-		return object : AsyncInputStream, AsyncLengthStream {
-			override suspend fun read(buffer: ByteArray, offset: Int, len: Int): Int = syncIS.read(buffer, offset, len)
-			override suspend fun read(): Int = syncIS.read()
-			override suspend fun close() = syncIS.close()
-			override suspend fun setLength(value: Long) {
-				unsupported("Can't set length")
-			}
-
+	return when {
+		length != null -> object : AsyncInputStream, AsyncLengthStream {
+			override suspend fun read(buffer: ByteArray, offset: Int, len: Int): Int = withContextNullable(context) { syncIS.read(buffer, offset, len) }
+			override suspend fun read(): Int = withContextNullable(context) { syncIS.read() }
+			override suspend fun close() = withContextNullable(context) { syncIS.close() }
+			override suspend fun setLength(value: Long): Unit = unsupported("Can't set length")
 			override suspend fun getLength(): Long = length
+			override suspend fun hasLength(): Boolean = true
 		}
-	} else {
-		return object : AsyncInputStream {
-			override suspend fun read(buffer: ByteArray, offset: Int, len: Int): Int = syncIS.read(buffer, offset, len)
-			override suspend fun read(): Int = syncIS.read()
-			override suspend fun close() { syncIS.close() }
+		else -> object : AsyncInputStream {
+			override suspend fun read(buffer: ByteArray, offset: Int, len: Int): Int = withContextNullable(context) { syncIS.read(buffer, offset, len) }
+			override suspend fun read(): Int = withContextNullable(context) { syncIS.read() }
+			override suspend fun close(): Unit = withContextNullable(context) { syncIS.close() }
 		}
 	}
 }
