19e7230be210c1cbf1733a21e1f52154868c4bfa
soywiz <soywiz@gmail.com>
2024-05-16 18:55:46 +0200

    WIP extract common JS/WASM defs to their own module

---
+++ korlibs-defs/src@js/korlibs/js/JSSymbol.kt
@@ -0,0 +1,3 @@
+package korlibs.js
+
+external val Symbol: dynamic
--- korlibs-io/src@js/korlibs/io/runtime/JsRuntimeDeno.kt
+++ korlibs-io/src@js/korlibs/io/runtime/JsRuntimeDeno.kt
@@ -184,15 +184,3 @@ class DenoAsyncStreamBase(val file: DenoFsFile) : AsyncStreamBase() {
         file.close()
     }
 }
-
-suspend fun <T> JSAsyncIterable<T>.toFlow(): Flow<T> = flow {
-    val iterator = (this@toFlow.asDynamic())[Symbol_asyncIterator]
-    val gen = iterator.call(this)
-    //println(gen)
-    while (true) {
-        val prom = gen.next().unsafeCast<Promise<JSIterableResult<T>>>()
-        val value = prom.await()
-        if (value.done) break
-        emit(value.value)
-    }
-}
