7dab930a1ca7b5f9f730e85670b68dc590a4cc8f
soywiz <soywiz@gmail.com>
2024-06-25 12:02:03 +0200

    Fixes NaN and Infinite values, and allow to get sign, exponent and mantissa from Double IEEE 754

--- korlibs-math-core/src/korlibs/memory/DoubleBits.kt
+++ korlibs-math-core/src/korlibs/memory/DoubleBits.kt
@@ -1,9 +1,42 @@
 package korlibs.memory
 
+// S | EEEEEEEEEEE | FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+// S=1
+// E=11
+// F=52
+
+fun Double.toStringInfo() = buildString(128) {
+    append(this@toStringInfo)
+    append(" = Double.fromParts(")
+    append("sign=")
+    append(this@toStringInfo.bitsSign)
+    append(", exponent=0b")
+    append(this@toStringInfo.bitsExponent.toString(2).padStart(11, '0'))
+    append(", mantissa=0b")
+    //append(this@toStringInfo.bitsMantissaLong.toString(2).padStart(52, '0'))
+    append(this@toStringInfo.bitsMantissaHigh.toString(2).padStart(20, '0'))
+    append(this@toStringInfo.bitsMantissaLow.toString(2).padStart(32, '0'))
+    append(")")
+}
+
+private const val TWO_POW_32_DOUBLE = 4294967296.0
+val Double.Companion.TWO_POW_32 get() = TWO_POW_32_DOUBLE
+
+fun Double.Companion.fromParts(sign: Int, exponent: Int, mantissa: Double): Double = fromParts(sign, exponent, (mantissa % TWO_POW_32_DOUBLE).toInt(), (mantissa / TWO_POW_32_DOUBLE).toInt())
+fun Double.Companion.fromParts(sign: Int, exponent: Int, mantissa: Long): Double = fromParts(sign, exponent, mantissa.low, mantissa.high)
+fun Double.Companion.fromParts(sign: Int, exponent: Int, mantissaLow: Int, mantissaHigh: Int): Double = fromLowHigh(mantissaLow, mantissaHigh.insert12(exponent, 20).insert1(sign, 31))
+
 expect fun Double.Companion.fromLowHigh(low: Int, high: Int): Double
 expect inline fun <T> Double.getLowHighBits(block: (low: Int, high: Int) -> T): T
 expect val Double.low: Int
 expect val Double.high: Int
+
+val Double.bitsSign: Int get() = high.extract1(31)
+val Double.bitsExponent: Int get() = high.extract11(20)
+val Double.bitsMantissaHigh: Int get() = high.extract20(0)
+val Double.bitsMantissaLow: Int get() = low
+val Double.bitsMantissaDouble: Double get() = bitsMantissaLow.toDouble() + bitsMantissaHigh.toDouble() * 4294967296.0
+val Double.bitsMantissaLong: Long get() = Long.fromLowHigh(bitsMantissaLow, bitsMantissaHigh)
 
 @PublishedApi internal fun Double.Companion.fromLowHighBitsSlow(low: Int, high: Int): Double = Double.fromBits(Long.fromLowHigh(low, high))
 @PublishedApi internal inline fun <T> Double.getLowHighBitsSlow(block: (low: Int, high: Int) -> T): T = block(lowSlow, highSlow)
--- korlibs-math-core/test/korlibs/memory/DoubleBitsTest.kt
+++ korlibs-math-core/test/korlibs/memory/DoubleBitsTest.kt
@@ -1,6 +1,7 @@
 package korlibs.memory
 
 import korlibs.platform.*
+import kotlin.random.*
 import kotlin.test.*
 import kotlin.time.*
 
@@ -52,6 +53,58 @@ class DoubleBitsTest {
 
             println("${Platform.rawPlatformName}[$M] : DOUBLE: $doubleTime, LONG: $longTime")
             //println("${Platform.rawPlatformName}[$M] : DOUBLE: $doubleTime")
+        }
+    }
+
+    @Test
+    fun testDoubleParts() {
+        assertEquals(1024.0, Double.fromParts(sign=0, exponent=0b10000001001, 0b0000000000000000000000000000000000000000000000000000))
+        assertEquals(-1024.0, Double.fromParts(sign=1, exponent=0b10000001001, 0b0000000000000000000000000000000000000000000000000000))
+        assertEquals(1536.0, Double.fromParts(sign=0, exponent=0b10000001001, mantissa=0b1000000000000000000000000000000000000000000000000000))
+        assertEquals(0.0, Double.fromParts(sign=0, exponent=0b00000000000, mantissa=0b0000000000000000000000000000000000000000000000000000))
+        assertEquals(1.0, Double.fromParts(sign=0, exponent=0b01111111111, mantissa=0b0000000000000000000000000000000000000000000000000000))
+        assertEquals(3.0, Double.fromParts(sign=0, exponent=0b10000000000, mantissa=0b1000000000000000000000000000000000000000000000000000))
+        assertEquals(7.76421, Double.fromParts(sign=0, exponent=0b10000000001, mantissa=0b1111000011101000110100010000111101010001101011001010))
+        assertEquals(Double.NEGATIVE_INFINITY, Double.fromParts(1, -1, 0L))
+        assertEquals(Double.POSITIVE_INFINITY, Double.fromParts(0, -1, 0L))
+        assertEquals(Double.NaN, Double.fromParts(1, -1, 1L))
+        assertEquals(Double.NaN, Double.fromParts(0, -1, 1L))
+    }
+
+    @Test
+    fun testEdgeNanValues() {
+        val random = Random(0L)
+
+        0xfffa9138acdc4aa6uL.toLong().also { invalidLong ->
+            val invalidDouble = Double.fromBits(invalidLong)
+            assertEquals(invalidLong.low, invalidDouble.low)
+            assertEquals(invalidLong.high, invalidDouble.high)
+        }
+
+        repeat(100000) {
+            val sign = 0
+            val exponent = 0b01111111111
+            val mantissaLow = random.nextBits(32)
+            val mantissaHigh = random.nextBits(20)
+            val double = Double.fromParts(sign, exponent, mantissaLow, mantissaHigh)
+            assertEquals(sign, double.bitsSign)
+            assertEquals(exponent, double.bitsExponent)
+            assertEquals(mantissaLow, double.bitsMantissaLow)
+            assertEquals(mantissaHigh, double.bitsMantissaHigh)
+        }
+        repeat(100000) {
+            val long = random.nextLong()
+            val low = long.low
+            val high = long.high
+            val double = Double.fromLowHigh(low, high)
+            //println("long=0x${long.toULong().toString(16).padStart(16, '0')}, double=$double")
+            assertEquals(low, double.low)
+            assertEquals(high, double.high)
+            /*
+            val double = long.reinterpretAsDouble()
+            val nlong = double.reinterpretAsLong()
+            assertEquals(long, nlong)
+            */
         }
     }
 }
