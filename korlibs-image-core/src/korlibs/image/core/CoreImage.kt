3f375beca2c9b784226de41d0613a64682c5be67
soywiz <soywiz@gmail.com>
2024-07-08 15:32:22 +0200

    Provide some documentation

--- korlibs-image-core/src/korlibs/image/core/CoreImage.kt
+++ korlibs-image-core/src/korlibs/image/core/CoreImage.kt
@@ -1,5 +1,8 @@
 package korlibs.image.core
 
+/**
+ * An image representation that can be used to encode/decode images in different formats.
+ */
 interface CoreImage {
     /** [width] of the image */
     val width: Int
@@ -17,7 +20,11 @@ interface CoreImage {
     companion object
 }
 
-/** In RGBA format */
+/**
+ * A 32-bit image representation.
+ *
+ * [data] is in [CoreImage32Color] format
+ */
 class CoreImage32(
     override val width: Int,
     override val height: Int,
@@ -29,6 +36,9 @@ class CoreImage32(
     override fun to32(): CoreImage32 = this
 }
 
+/**
+ * 32-bit RGBA color format. Used in [CoreImage32]
+ */
 inline class CoreImage32Color(val value: Int) {
     constructor(red: UByte, green: UByte, blue: UByte, alpha: UByte = 255u) : this((red.toInt() shl RED_OFFSET) or (green.toInt() shl GREEN_OFFSET) or (blue.toInt() shl BLUE_OFFSET) or (alpha.toInt() shl ALPHA_OFFSET))
     constructor(red: Int, green: Int, blue: Int, alpha: Int = 255) : this(red.coerceIn(0, 255).toUByte(), green.coerceIn(0, 255).toUByte(), blue.coerceIn(0, 255).toUByte(), alpha.coerceIn(0, 255).toUByte())
@@ -46,16 +56,28 @@ inline class CoreImage32Color(val value: Int) {
     }
 }
 
+/**
+ * Provides information about an image.
+ */
 data class CoreImageInfo(
+    /** [width] of the image */
     val width: Int,
+    /** [height] of the image */
     val height: Int,
+    /** Bits per pixel */
     val bpp: Int = 32,
     val format: CoreImageFormat? = null,
     val premultiplied: Boolean = true,
 )
 
+/**
+ * Provides information about an image.
+ */
 fun CoreImage.info(): CoreImageInfo = CoreImageInfo(width, height, bpp, format = null, premultiplied = premultiplied)
 
+/**
+ * Image format: [PNG], [JPEG], [WEBP], [AVIF], etc.
+ */
 inline class CoreImageFormat(val name: String) {
     companion object {
         val PNG = CoreImageFormat("PNG")
@@ -65,6 +87,9 @@ inline class CoreImageFormat(val name: String) {
     }
 }
 
+/**
+ * Provides image encoding/decoding capabilities.
+ */
 interface CoreImageFormatProvider {
     /**
      * Gets the [CoreImageInfo] of a [data] ByteArray. Potentially without decoding the pixels.
@@ -80,21 +105,35 @@ interface CoreImageFormatProvider {
      * Encodes a [CoreImage] into a [ByteArray] in the specified [format] (PNG, JPEG, etc.)
      */
     suspend fun encode(image: CoreImage, format: CoreImageFormat, level: Double = 1.0): ByteArray
+
+    companion object
 }
 
 expect val CoreImageFormatProvider_default: CoreImageFormatProvider
 
 private var _CoreImageFormatProvider_current: CoreImageFormatProvider? = null
 
-var CoreImageFormatProvider_current: CoreImageFormatProvider
+/**
+ * Current [CoreImageFormatProvider] used by [CoreImage] operations. By default, it uses the default one from the platform.
+ */
+var CoreImageFormatProvider.Companion.CURRENT: CoreImageFormatProvider
     get() = _CoreImageFormatProvider_current ?: CoreImageFormatProvider_default
     set(value) { _CoreImageFormatProvider_current = value }
 
+/**
+ * Gets the [CoreImageInfo] of a [data] ByteArray. Potentially without decoding the pixels.
+ */
 suspend fun CoreImage.Companion.info(data: ByteArray): CoreImageInfo =
-    CoreImageFormatProvider_current.info(data)
+    CoreImageFormatProvider.CURRENT.info(data)
 
+/**
+ * Decodes a [data] ByteArray into a CoreImage
+ */
 suspend fun CoreImage.Companion.decodeBytes(data: ByteArray): CoreImage =
-    CoreImageFormatProvider_current.decode(data)
+    CoreImageFormatProvider.CURRENT.decode(data)
 
+/**
+ * Encodes a [CoreImage] into a [ByteArray] in the specified [format] (PNG, JPEG, etc.)
+ */
 suspend fun CoreImage.encodeBytes(format: CoreImageFormat, level: Double = 1.0): ByteArray =
-    CoreImageFormatProvider_current.encode(this, format, level)
+    CoreImageFormatProvider.CURRENT.encode(this, format, level)
