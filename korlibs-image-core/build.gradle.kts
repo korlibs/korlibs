a4ba895a17130a9fce9f8b800e2815cf1187f622
soywiz <soywiz@gmail.com>
2024-07-08 13:39:55 +0200

    Encode in Win32CoreImageFormatProvider via stbi for now

---
+++ korlibs-image-core/build.gradle.kts
@@ -0,0 +1,14 @@
+import org.jetbrains.kotlin.gradle.plugin.*
+import org.jetbrains.kotlin.gradle.plugin.mpp.*
+
+afterEvaluate {
+    kotlin.targets.filter { it.platformType == KotlinPlatformType.native }.forEach { target ->
+        target.compilations.getByName("main") {
+            (this as KotlinNativeCompilation).cinterops {
+                val stb_image by creating {
+                    defFile(project.file("nativeInterop/cinterop/stb_image.def"))
+                }
+            }
+        }
+    }
+}
--- korlibs-image-core/test/korlibs/image/core/CoreImageTest.kt
+++ korlibs-image-core/test/korlibs/image/core/CoreImageTest.kt
@@ -5,16 +5,24 @@ import kotlin.io.encoding.*
 import kotlin.test.*
 
 @OptIn(ExperimentalEncodingApi::class)
-class DecodeTest {
+class CoreImageTest {
     val pngData = Base64.decode("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=")
 
     @Test
     fun testInfo() = runTest {
-        assertEquals(CoreImageInfo(1, 1), CoreImage.info(pngData))
+        assertEquals(CoreImageInfo(1, 1), CoreImage.info(pngData).copy(format = null))
     }
 
     @Test
-    fun test() = runTest {
+    fun testDecode() = runTest {
         assertEquals(CoreImageInfo(1, 1), CoreImage.decodeBytes(pngData).info())
+    }
+
+    @Test
+    fun testEncode() = runTest {
+        assertEquals(
+            CoreImageInfo(2, 1),
+            CoreImage.decodeBytes(CoreImage32(2, 1, intArrayOf(-1, -1)).encodeBytes(CoreImageFormat.PNG)).info()
+        )
     }
 }
