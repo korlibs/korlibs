1bd9c2d0da353e3c9a57a9571333e321af61c24b
soywiz <soywiz@gmail.com>
2024-07-09 11:42:00 +0200

    Do not do C Interop commonization, and include stbi on mingw and linux

--- korlibs-image-core/build.gradle.kts
+++ korlibs-image-core/build.gradle.kts
@@ -3,10 +3,12 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.*
 
 afterEvaluate {
     kotlin.targets.filter { it.platformType == KotlinPlatformType.native }.forEach { target ->
-        target.compilations.getByName("main") {
-            (this as KotlinNativeCompilation).cinterops {
-                val stb_image by creating {
-                    defFile(project.file("nativeInterop/cinterop/stb_image.def"))
+        if (target.name.contains("linux") || target.name.contains("mingw")) {
+            target.compilations.getByName("main") {
+                (this as KotlinNativeCompilation).cinterops {
+                    val stb_image by creating {
+                        defFile(project.file("nativeInterop/cinterop/stb_image.def"))
+                    }
                 }
             }
         }
