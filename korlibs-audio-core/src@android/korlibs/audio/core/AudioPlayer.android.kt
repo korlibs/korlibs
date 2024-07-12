4b7251c1e839a71b90e453023a9b879acf37e8b4
soywiz <soywiz@gmail.com>
2024-07-12 13:50:50 +0200

    Implement AndroidAudioSystem

--- korlibs-audio-core/src@android/korlibs/audio/core/AudioPlayer.android.kt
+++ korlibs-audio-core/src@android/korlibs/audio/core/AudioPlayer.android.kt
@@ -1,5 +1,92 @@
 package korlibs.audio.core
 
+import android.content.*
+import android.media.*
+import android.os.*
 import korlibs.audio.core.impl.*
+import korlibs.io.android.*
+import kotlin.coroutines.*
 
-actual val defaultAudioSystem: AudioSystem = NullAudioSystem
+actual val defaultAudioSystem: AudioSystem = AndroidAudioSystem
+
+object AndroidAudioSystem : AudioSystem() {
+    internal var audioSessionId: Int = -1 ; private set
+    internal lateinit var audioManager: AudioManager
+
+    override fun initialize(context: CoroutineContext) {
+        if (::audioManager.isInitialized) return
+        val ctx = context[AndroidCoroutineContext.Key]?.context ?: error("Can't find the Android Context on the CoroutineContext. Must call withAndroidContext first")
+        audioManager = ctx.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) audioSessionId = audioManager!!.generateAudioSessionId()
+    }
+
+    override fun createPlayer(device: AudioDevice): AudioPlayer = SoftAudioPlayer(device, AndroidAudioStreamPlayer(this))
+}
+
+class AndroidAudioStreamPlayer(val system: AndroidAudioSystem) : AudioStreamPlayer {
+    override fun playStream(device: AudioDevice, rate: Int, channels: Int, gen: (position: Long, data: SeparatedAudioSamples) -> Int): AudioSimpleStream {
+        var paused = false
+        val thread = korlibs.concurrent.thread.nativeThread(isDaemon = true) { thread ->
+            //val bufferSamples = 4096
+            val bufferSamples = 1024
+
+            val atChannelSize = Short.SIZE_BYTES * channels * bufferSamples
+            val atChannel = if (channels >= 2) AudioFormat.CHANNEL_OUT_STEREO else AudioFormat.CHANNEL_OUT_MONO
+            val atMode = AudioTrack.MODE_STREAM
+            val at = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && system.audioSessionId >= 0) {
+                AudioTrack(
+                    //.setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_GAME).setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN).build(),
+                    AudioFormat.Builder().setChannelMask(atChannel).setSampleRate(rate).setEncoding(AudioFormat.ENCODING_PCM_16BIT).build(),
+                    atChannelSize,
+                    atMode,
+                    system.audioSessionId
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                AudioTrack(AudioManager.STREAM_MUSIC, rate, atChannel, AudioFormat.ENCODING_PCM_16BIT, atChannelSize, atMode)
+            }
+            if (at.state == AudioTrack.STATE_UNINITIALIZED) {
+                System.err.println("Audio track was not initialized correctly frequency=$rate, bufferSamples=$bufferSamples")
+            }
+
+            val buffer = SeparatedAudioSamples(channels, bufferSamples)
+            val interleaved = buffer.interleaved()
+            at.play()
+
+            try {
+                var position = 0L
+                while (thread.threadSuggestRunning) {
+                    if (paused) {
+                        at.pause()
+                        Thread.sleep(20L)
+                        continue
+                    } else {
+                        at.play()
+                    }
+
+                    when (at.state) {
+                        AudioTrack.STATE_UNINITIALIZED -> Thread.sleep(20L)
+                        AudioTrack.STATE_INITIALIZED -> {
+                            at.playbackRate = rate
+                            position += gen(position, buffer)
+                            buffer.interleaved(interleaved)
+                            at.write(interleaved.asShortArray(), 0, interleaved.size)
+                        }
+                    }
+                }
+            } finally {
+                at.flush()
+                at.stop()
+                at.release()
+            }
+
+            //val temp = AudioSamplesInterleaved(2, bufferSamples)
+        }
+
+        return AudioSimpleStream(
+            onPausedChange = { paused = it },
+            onClosed = { thread.threadSuggestRunning = false }
+        )
+    }
+}
