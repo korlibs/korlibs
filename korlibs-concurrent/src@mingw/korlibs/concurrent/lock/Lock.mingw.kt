67f61c7e5cba704de54c5faff02177588bd68759
soywiz <soywiz@gmail.com>
2024-07-15 20:41:57 +0200

    Implement Lock in windows without pthreads and native windows calls (getting a much better performance and timing accuracy like on macos and linux)

--- korlibs-concurrent/src@mingw/korlibs/concurrent/lock/Lock.mingw.kt
+++ korlibs-concurrent/src@mingw/korlibs/concurrent/lock/Lock.mingw.kt
@@ -3,8 +3,7 @@ package korlibs.concurrent.lock
 import korlibs.time.*
 import kotlinx.atomicfu.*
 import kotlinx.cinterop.*
-import platform.posix.*
-import kotlin.time.*
+import platform.windows.*
 
 @OptIn(ExperimentalForeignApi::class)
 actual class Lock actual constructor() : BaseLockWithNotifyAndWait {
@@ -13,8 +12,8 @@ actual class Lock actual constructor() : BaseLockWithNotifyAndWait {
     private val nrlock = NonRecursiveLock()
     private val nlocks = atomic(0)
     private var arena: Arena? = null
-    private var mutex: LongVarOf<pthread_mutex_t>? = null
-    private var cond: LongVarOf<pthread_cond_t>? = null
+    private var mutex: CRITICAL_SECTION? = null
+    private var cond: CONDITION_VARIABLE? = null
 
     actual override fun lock() {
         val mut = nrlock {
@@ -23,31 +22,31 @@ actual class Lock actual constructor() : BaseLockWithNotifyAndWait {
                 arena = Arena()
             }
             if (cond == null) {
-                cond = arena!!.alloc<pthread_cond_tVar>().also {
-                    pthread_cond_init(it.ptr, null)
+                cond = arena!!.alloc<CONDITION_VARIABLE>().also {
+                    InitializeConditionVariable(it.ptr)
                     //pthread_cond_destroy(cond.ptr)
                 }
             }
             if (mutex == null) {
-                mutex = arena!!.alloc<pthread_mutex_tVar>().also {
-                    pthread_mutex_init(it.ptr, null)
+                mutex = arena!!.alloc<CRITICAL_SECTION>().also {
+                    InitializeCriticalSection(it.ptr)
                     //pthread_mutex_destroy(mutex.ptr)
                 }
             }
             nlocks.incrementAndGet()
             mutex!!
         }
-        pthread_mutex_lock(mut.ptr)
+        EnterCriticalSection(mut.ptr)
     }
 
     actual override fun unlock() {
         nrlock {
         //run {
-            pthread_mutex_unlock(mutex!!.ptr)
+            LeaveCriticalSection(mutex!!.ptr)
 
             if (nlocks.decrementAndGet() == 0) {
-                if (cond != null) pthread_cond_destroy(cond?.ptr)
-                if (mutex != null) pthread_mutex_destroy(mutex?.ptr)
+                //if (cond != null) DeleteConditionVariable(cond?.ptr)
+                if (mutex != null) DeleteCriticalSection(mutex?.ptr)
                 arena?.clear()
                 cond = null
                 mutex = null
@@ -58,37 +57,16 @@ actual class Lock actual constructor() : BaseLockWithNotifyAndWait {
 
     actual override fun notify(unit: Unit) {
         //println("SIGNALING...")
-        pthread_cond_signal(cond!!.ptr)
+        WakeConditionVariable(cond!!.ptr)
         //println(" --> SIGNALED")
         //pthread_cond_broadcast() // notifyall
-    }
-
-    fun timespec.toDuration(): Duration = (tv_sec.seconds + tv_nsec.nanoseconds)
-    fun Duration.toTimespec(out: timespec): timespec {
-        val nanos = inWholeNanoseconds
-        val s: Long = (nanos / 1_000_000_000)
-        val n: Long = (nanos % 1_000_000_000)
-        out.tv_sec = s
-        out.tv_nsec = n.convert()
-        return out
     }
 
     actual override fun wait(time: FastDuration): Boolean {
         val time = time.slow
 
         return memScoped {
-            var tspec: timespec = alloc<timespec>()
-            clock_gettime(CLOCK_REALTIME, tspec.ptr)
-            val clockTime = tspec.toDuration()
-            //val waitTime = (clockTime + time - 10.milliseconds)
-            val waitTime = (clockTime + time)
-            tspec = waitTime.toTimespec(tspec)
-            //println("WAITING... $time")
-            //val startTime = CoreTime.currentTimeMillisDouble()
-            (pthread_cond_timedwait(cond!!.ptr, mutex!!.ptr, tspec.ptr)).also {
-                //val elapsedTime = (CoreTime.currentTimeMillisDouble() - startTime).milliseconds
-                //println(" --> WAITED it=$it : timedout=${it == ETIMEDOUT}, elapsedTime=$elapsedTime, clockTime=$clockTime, waitTime=$waitTime -- ${waitTime - clockTime}, requested=${time}")
-            } == ETIMEDOUT
+            SleepConditionVariableCS(cond!!.ptr, mutex!!.ptr, time.millisecondsInt.convert()) == ERROR_TIMEOUT
         }
     }
 }
