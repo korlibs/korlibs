9f88561acf7db0dc344245c27c673369ec5f431c
soywiz <soywiz@gmail.com>
2024-07-16 16:38:30 +0200

    Minor

--- korlibs-audio-core/src/korlibs/audio/core/impl/Win32AudioSystem.kt
+++ korlibs-audio-core/src/korlibs/audio/core/impl/Win32AudioSystem.kt
@@ -4,7 +4,6 @@ import korlibs.audio.core.*
 import korlibs.audio.sound.*
 import korlibs.concurrent.thread.*
 import korlibs.ffi.*
-import korlibs.io.lang.*
 import korlibs.memory.*
 import korlibs.time.*
 
@@ -92,7 +91,7 @@ internal object Win32AudioSystem : AudioSystem() {
                     }
                 }
             }
-            return Closeable {
+            return AutoCloseable {
                 running = false
             }
         }
