70f70390c7e3bae0b58433248d666d22cbd2ef12
soywiz <soywiz@gmail.com>
2024-08-06 08:57:42 +0200

    Add sync task

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,33 +8,3 @@ pluginManagement {
         //maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
-
-plugins {
-    //id("org.jetbrains.amper.settings.plugin").version("0.2.1-dev-470")
-    //id("org.jetbrains.amper.settings.plugin").version("0.2.2")
-    //id("org.jetbrains.amper.settings.plugin").version("0.2.3-dev-473")
-}
-
-for (file in rootDir.listFiles()) {
-    if (file.isDirectory() && File(file, "module.yaml").exists()) {
-        include(":${file.name}")
-        project(":${file.name}").projectDir = file
-    }
-}
-
-rootProject.name = "korlibs"
-
-File(rootDir, "local.properties").also { localProperties ->
-    if (localProperties.exists()) return@also
-
-    fun detectAndroidHome(): File? = listOf(
-        System.getenv("ANDROID_HOME")?.let { File(it) },
-        File(System.getProperty("user.home"), "/Library/Android/sdk"),
-        File(System.getProperty("user.home"), "/Android/Sdk"),
-        File(System.getProperty("user.home"), "/AppData/Local/Android/Sdk"),
-    ).firstNotNullOfOrNull { it?.takeIf { it.isDirectory } }
-
-    detectAndroidHome()?.let {
-        localProperties.writeText("sdk.dir=${it.absolutePath.replace("\\", "\\\\").replace(":", "\\:")}")
-    }
-}
