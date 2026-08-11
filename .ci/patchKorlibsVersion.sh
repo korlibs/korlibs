d12e16f3e10f0271491ca6f8732841e2f64b3408
Marko Koschak <marko.koschak@tisno.de>
2026-08-11 21:37:22 +0200

    fix: Update CI configuration and add script to patch Korlibs version for local testing

---
+++ .ci/patchKorlibsVersion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script is used on CI (ubuntu-korge-unit-test) to patch the version string for local releasing
+
+echo "${{ github.event.pull_request.body }}"
+
+# Clone locally Korge repo for running linux unit tests with locally released Korlibs version
+rm -rf dep-korge
+mkdir dep-korge
+git clone https://github.com/korlibs/korge.git dep-korge
+git checkout main
+
+# Create Git-Hash (short form)
+GIT_HASH=$(git rev-parse --short HEAD)
+
+# Patch Korlibs version SNAPSHOT with Git-Hash
+sed -i "s/SNAPSHOT/$GIT_HASH/" gradle/libs.versions.toml
+sed -i "s/SNAPSHOT/$GIT_HASH/" dep-korge/gradle/libs.versions.toml
+
+echo "✓ Korlibs version updated: SNAPSHOT → $GIT_HASH"
+
+#echo "export GIT_HASH=$GIT_HASH" >.ci/gitHashKorlibs
